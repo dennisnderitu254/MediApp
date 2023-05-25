d98fed65e660c0744132587f74cd5977e0312658
dennisnderitu254 <dknderitu@gmail.com>
2023-05-25 13:38:02 +0300

    working on approutes and templates

--- app.py
+++ app.py
@@ -11,45 +11,37 @@ app = Flask(__name__, template_folder='templates')
 app.secret_key = 'kenya1234'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root1234'
-app.config['MYSQL_DB'] = 'mediappdb'
+app.config['MYSQL_PASSWORD'] = '----'
+app.config['MYSQL_DB'] = '--------'
 
 mysql = MySQL(app)
 
 
+@app.route('/')
 @app.route('/home')
 def home():
     message = 'Welcome to MediApp!!'
     return render_template('home.html', message=message)
 
 
+@app.route('/loginp')
 @app.route('/loginpatient')
 def login_patient():
     message = 'Welcome User!'
     return render_template('login_patient.html', message=message)
 
 
+@app.route('/logind')
 @app.route('/logindoctor')
 def login_doctor():
     message = 'Welcome Doctor'
     return render_template('login_doctor.html', message=message)
 
 
-@app.route('/registerpatient')
-def register_patient():
-    message = 'Welcome Patient'
-    return render_template('registration_patient.html', message=message)
-
-
-@app.route('/registerdoctor')
-def register_doctor():
-    message = 'Welcome Doctor'
-    return render_template('registration_doctor.html', message=message)
-
-
-# @app.route('/admin')
-# def admin_login():
-#     return render_template('admin_login.html')
+@app.route('/loginadmin')
+@app.route('/admin')
+def admin_login():
+    return render_template('admin_login.html')
 
 
 @app.route('/logout')
@@ -84,7 +76,7 @@ if __name__ == "__main__":
 #             return render_template('user.html', mesage=mesage)
 #         else:
 #             mesage = 'Please enter correct email / password !'
-#     return render_template('login_patient.html', mesage=mesage)
+#     return render_template('login_patient.html', message=message)
 
 
 # @app.route('/logindoctor', methods=['GET', 'POST'])
@@ -109,64 +101,64 @@ if __name__ == "__main__":
 #     return render_template('login_doctor.html', mesage=mesage)
 
 
-# @app.route('/registerpatient', methods=['GET', 'POST'])
-# def register_patient():
-#     mesage = ''
-#     if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form:
-#         firstname = request.form['firstname']
-#         lastname = request.form['lastname']
-#         dateofbirth = request.form['dateofbirth']
-#         gender = request.form['gender']
-#         phonenumber = request.form['phonenumber']
-#         username = request.form['username']
-#         password = request.form['password']
-#         email = request.form['email']
-#         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-#         cursor.execute('SELECT * FROM patient WHERE email = % s', (email, ))
-#         account = cursor.fetchone()
-#         if account:
-#             mesage = 'Account already exists !'
-#         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-#             mesage = 'Invalid email address !'
-#         elif not username or not password or not email:
-#             mesage = 'Please fill out the form !'
-#         else:
-#             cursor.execute(
-#                 'INSERT INTO patient VALUES (NULL, % s, % s, % s, %s, %s, %s, %s, %s)', (firstname, lastname, dateofbirth, gender, phonenumber, username, email, password, ))
-#             mysql.connection.commit()
-#             mesage = 'You have successfully registered !'
-#     elif request.method == 'POST':
-#         mesage = 'Please fill out the form !'
-#     return render_template('registration_patient.html', mesage=mesage)
+@app.route('/registerpatient', methods=['GET', 'POST'])
+def register_patient():
+    mesage = ''
+    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form:
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        dateofbirth = request.form['dateofbirth']
+        gender = request.form['gender']
+        phonenumber = request.form['phonenumber']
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM patient WHERE email = % s', (email, ))
+        account = cursor.fetchone()
+        if account:
+            message = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            message = 'Invalid email address !'
+        elif not username or not password or not email:
+            message = 'Please fill out the form !'
+        else:
+            cursor.execute(
+                'INSERT INTO patient VALUES (NULL, % s, % s, % s, %s, %s, %s, %s, %s)', (firstname, lastname, dateofbirth, gender, phonenumber, username, email, password, ))
+            mysql.connection.commit()
+            message = 'You have successfully registered !'
+    elif request.method == 'POST':
+        message = 'Please fill out the form !'
+    return render_template('registration_patient.html', message=message)
 
 
-# @app.route('/registerdoctor', methods=['GET', 'POST'])
-# def register_doctor():
-#     mesage = ''
-#     if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form:
-#         firstname = request.form['firstname']
-#         lastname = request.form['lastname']
-#         dateofbirth = request.form['dateofbirth']
-#         gender = request.form['gender']
-#         phonenumber = request.form['phonenumber']
-#         specialization = request.form['specialization']
-#         username = request.form['username']
-#         password = request.form['password']
-#         email = request.form['email']
-#         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-#         cursor.execute('SELECT * FROM patient WHERE email = % s', (email, ))
-#         account = cursor.fetchone()
-#         if account:
-#             mesage = 'Account already exists !'
-#         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-#             mesage = 'Invalid email address !'
-#         elif not username or not password or not email:
-#             mesage = 'Please fill out the form !'
-#         else:
-#             cursor.execute(
-#                 'INSERT INTO patient VALUES (NULL, % s, % s, % s, %s, %s, %s, %s, %s)', (firstname, lastname, dateofbirth, gender, phonenumber, specialization, username, email, password, ))
-#             mysql.connection.commit()
-#             mesage = 'You have successfully registered !'
-#     elif request.method == 'POST':
-#         mesage = 'Please fill out the form !'
-#     return render_template('registration_doctor.html', mesage=mesage)
+@app.route('/registerdoctor', methods=['GET', 'POST'])
+def registerdoctor():
+    message = ''
+    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form:
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        dateofbirth = request.form['dateofbirth']
+        gender = request.form['gender']
+        phonenumber = request.form['phonenumber']
+        specialization = request.form['specialization']
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM patient WHERE email = % s', (email, ))
+        account = cursor.fetchone()
+        if account:
+            message = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            message = 'Invalid email address !'
+        elif not username or not password or not email:
+            message = 'Please fill out the form !'
+        else:
+            cursor.execute(
+                'INSERT INTO patient VALUES (NULL, % s, % s, % s, %s, %s, %s, %s, %s)', (firstname, lastname, dateofbirth, gender, phonenumber, specialization, username, email, password, ))
+            mysql.connection.commit()
+            message = 'You have successfully registered !'
+    elif request.method == 'POST':
+        message = 'Please fill out the form !'
+    return render_template('registration_doctor.html', message=message)
