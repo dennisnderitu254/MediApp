317808eff5a1fe09a3a9452be3939ad4e5623821
dennisnderitu254 <dknderitu@gmail.com>
2023-06-12 11:56:02 +0300

    adding login functionality to app

--- app.py
+++ app.py
@@ -19,6 +19,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SelectField
 from wtforms.validators import DataRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 app = Flask(__name__)
@@ -27,6 +30,12 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey1234'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root1234@localhost/mediappnewdb'
 db = SQLAlchemy(app)
+
+# Configure MySQL connection
+mysql_host = 'localhost'
+mysql_user = 'root'
+mysql_password = 'root1234'
+mysql_database = 'mediappnewdb'
 
 
 # class LoginForm(FlaskForm):
@@ -40,6 +49,33 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[
                              DataRequired(), Length(min=6)])
+
+
+def validate_user_credentials(email, password):
+    try:
+        # Establish connection to the MySQL database
+        conn = sqlalchemy.create_engine(
+            f'mysql+mysqlconnector://{mysql_user}:{mysql_password}@{mysql_host}/{mysql_database}'
+        )
+
+        # Execute a query to retrieve the user's credentials
+        query = f"SELECT * FROM users WHERE email = '{email}'"
+        result = conn.execute(query)
+
+        # Fetch the user record
+        user = result.fetchone()
+
+        # Validate the user's credentials
+        if user and password == user.password:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        # Close the database connection
+        if conn:
+            conn.dispose()
 
 
 ################## ROUTING FUNCTIONS #########################
@@ -73,12 +109,31 @@ def user_register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def user_login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        account_type = form.account_type.data
+        email = form.email.data
+        password = form.password.data
+
+        # Validate the user's credentials against the MySQL database
+        if validate_user_credentials(email, password):
+            # Redirect to a success page or perform further actions
+            return redirect('/success')
+        else:
+            # Handle failed login attempts
+            error_message = 'Invalid email or password. Please try again.'
+            return render_template('login.html', form=form, error_message=error_message)
+    return render_template('login.html', form=form)
 
 
 @app.route('/registration_success')
 def registration_success():
     return 'User registered successfully'
+
+
+@app.route('/success')
+def success():
+    return "Login successful!"
 
 
 if __name__ == '__main__':
